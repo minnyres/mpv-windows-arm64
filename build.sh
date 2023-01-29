961358296778501ba351d912eb8b0e7432c4ec3f
minnyres <liuthgw23@gmail.com>
2023-01-29 14:24:27 +0800

    improve the package procedure

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ mpv_ver=v0.35.1
 
 workdir=$(pwd)
 
-pacman -S --needed git python mingw-w64-${runtime}-{clang,pkg-config,ffmpeg,libjpeg-turbo,lua51}
+pacman -S --needed git python p7zip mingw-w64-${runtime}-{clang,pkg-config,ffmpeg,libjpeg-turbo,lua51}
 
 mkdir -p src
 cd src
@@ -20,4 +20,7 @@ sed -i '6s/^/#undef MemoryBarrier\n/' ./video/out/opengl/ra_gl.c
 /usr/bin/python3 waf install
 
 cd $workdir/install/mpv/bin
-cp $(ldd mpv.exe | grep -v /c/WINDOWS) ./
+ldd mpv.exe | awk '{print $3}'| grep clangarm64 | xargs -I{} cp -u {} .
+
+cd $workdir/install
+7z a -mx9  mpv_${mpv_ver}_arm64.7z mpv
