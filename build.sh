a38441f3c552744d8c6b56fb2ead42b6031d7c69
minnyres <liuthgw23@gmail.com>
2023-11-24 19:56:39 +0800

    build mpv 0.37.0 with libplacebo

--- build.sh
+++ build.sh
@@ -2,25 +2,49 @@
 
 set -e
 runtime=clang-aarch64
-mpv_ver=v0.36.0
+mpv_ver=v0.37.0
+libplacebo_ver=v6.338.1
 
 workdir=$(pwd)
 
-pacman -S --needed git p7zip mingw-w64-${runtime}-{clang,pkg-config,ffmpeg,libjpeg-turbo,lua51,python,meson,rubberband,cmake,libva}
+pacman -S --needed git p7zip mingw-w64-${runtime}-{clang,pkgconf,ffmpeg,libjpeg-turbo,luajit,uchardet,python,meson,rubberband,cmake,libva,spirv-cross,shaderc,lcms2,libdovi,vulkan,glslang,xxhash,egl-headers,angleproject}
 
+# fetch sources
 mkdir -p src
 cd src
 git clone --depth 1 --branch $mpv_ver https://github.com/mpv-player/mpv.git mpv_$mpv_ver
+git clone --depth 1 --branch $libplacebo_ver https://code.videolan.org/videolan/libplacebo libplacebo_$libplacebo_ver
+
+# build libplacebo
+pushd libplacebo_$libplacebo_ver
+git submodule update --init
+dependsDir="$workdir/install/depends"
+meson setup build --prefix=$dependsDir \
+    --buildtype release --strip \
+    -Ddemos=false \
+    -Dtests=false \
+    -Dd3d11=enabled \
+    -Dlcms=enabled \
+    -Dlibdovi=enabled \
+    -Dshaderc=enabled \
+    -Dvulkan=enabled 
+meson compile -C build
+meson install -C build
+popd
+
+# build mpv
 cd mpv_$mpv_ver
 
 sed -i '6s/^/#undef MemoryBarrier\n/' ./video/out/opengl/ra_gl.c
-
+export PKG_CONFIG_PATH="$dependsDir/lib/pkgconfig"
+export PATH="$dependsDir/bin:$PATH"
 meson setup build -Dlibmpv=true --prefix=$workdir/install/mpv --buildtype release --strip
 meson compile -C build
 meson install -C build
 
 cd $workdir/install/mpv/bin
 ldd mpv.exe | awk '{print $3}'| grep clangarm64 | xargs -I{} cp -u {} .
+ldd mpv.exe | awk '{print $3}'| grep $dependsDir | xargs -I{} cp -u {} .
 
 cd $workdir/install
 7z a -mx9  mpv_${mpv_ver}_arm64.7z mpv
